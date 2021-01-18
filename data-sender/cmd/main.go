6f9cee1a725005bee606c9e06ed8bb357e384686
dawidkruk <d.kruk95@gmail.com>
2021-01-18 21:52:03 +0100

    Added the Dockerfile to build the image with data-sender

---
+++ data-sender/cmd/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s MESSAGE: %s \n", msg.Topic(), msg.Payload())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func main() {
+	// WHERE TO CONNECT TO
+
+	//var broker = "mosquitto-broker.default.svc.cluster.local"
+	var broker = "broker.hivemq.com"
+	var port = 1883
+
+	// GET THE HOSTNAME
+
+	hostname, err := os.Hostname()
+	if err == nil {
+		fmt.Println("hostname:", hostname)
+	}
+	hostname += "-sender-second"
+	//fmt.Println("var1 = ", reflect.ValueOf(hostname).Kind())
+
+	// DO SOME STUFF
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID(hostname) // TEMPORARY CLIENTID
+	//opts.SetUsername("dawidkruk-sender")
+	//opts.SetPassword("zxczxc")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	sub(client)
+	publish(client, hostname)
+
+	// DIRTY PLAY :V
+	for {
+	}
+
+}
+
+// func publish(client mqtt.Client, hostname string) {
+// 	i := 0
+// 	for {
+// 		text := fmt.Sprintf("Hello there #%d, USERNAME: %s", i, hostname)
+// 		token := client.Publish("dawidkruk/messages", 0, false, text)
+// 		token.Wait()
+// 		time.Sleep(500 * time.Millisecond)
+// 		i++
+// 	}
+// }
+
+func publish(client mqtt.Client, hostname string) {
+	i := 0
+	for {
+		text := fmt.Sprintf("SOMETHING")
+		token := client.Publish("dawidkruk/messages", 0, false, text)
+		token.Wait()
+		time.Sleep(500 * time.Millisecond)
+		i++
+	}
+}
+
+func sub(client mqtt.Client) {
+	topic := "dawidkruk/messages"
+	token := client.Subscribe(topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s \n", topic)
+}
---
+++ data-sender/go.mod
@@ -0,0 +1,5 @@
+module raven-point/data-collector
+
+go 1.15
+
+require github.com/eclipse/paho.mqtt.golang v1.3.1
